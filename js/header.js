e28ab6bc65d4ff199ac6a952db4fe64c32368aec
IsraelOliveira <104089819+Israel-Oliveira@users.noreply.github.com>
2022-09-13 20:58:07 -0300

    Atualizacao mobile
    Atualizacao do header no mobile

--- js/header.js
+++ js/header.js
@@ -1,12 +1,18 @@
 const checkbox = document.getElementById('checkbox')
 const menu = document.querySelector('.menu')
+const hdescondido = document.querySelector('header')
+const logoescondida = document.querySelector('.logo')
 
 checkbox.addEventListener('change', (checado) =>{
   if(checado.currentTarget.checked){
     menu.classList.remove('class', 'menu')
     menu.classList.add('class', 'menu_excondido')
+    hdescondido.classList.toggle('hd-expanded')
+    logoescondida.classList.add('logo-expanded')
   }else{
     menu.classList.remove('class', 'menu_excondido')
     menu.classList.add('class', 'menu')
+    hdescondido.classList.toggle('hd-expanded')
+    logoescondida.classList.remove('logo-expanded')
   }
 })
